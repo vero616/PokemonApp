71e960029516bf0e114cef19ab7a38d60e907db1
vero <veronique.hager@icloud.com>
2025-02-03 18:00:27 -0700

    added titles

--- PokemonApp/Controllers/Core/PTabViewController.swift
+++ PokemonApp/Controllers/Core/PTabViewController.swift
@@ -11,7 +11,30 @@ class PTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .magenta
+        setUpTabs()
+    }
+    
+    private func setUpTabs() {
+        let pokemonVC = PPokemonViewController()
+        let evolutionsVC = PEvolutionsViewController()
+        let gamesVC = PGamesViewController()
+        let settingsVC = PSettingsViewController()
+        
+        pokemonVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: pokemonVC)
+        let nav2 = UINavigationController(rootViewController: evolutionsVC)
+        let nav3 = UINavigationController(rootViewController: gamesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers(
+        [nav1, nav2, nav3, nav4],
+        animated: true
+        )
     }
 
 
